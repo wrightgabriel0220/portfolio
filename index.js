b5d3bcf404178288638c281de902b6b3c98aec8b
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-11-18 09:36:02 -0800

    Implemented virtual cursor resizing on button hover

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const cursor = document.getElementById('crosshair-cursor');
 const body = document.getElementsByTagName('html')[0];
+const buttons = document.getElementsByTagName('button');
 
-console.log('Running script');
-console.log(body);
+const updateCursorSize = targetSize => {
+  cursor.style.height = targetSize + 'px';
+  cursor.style.width = targetSize + 'px';
+}
 
 body.onmousemove = event => {
-  cursor.style.left = (event.clientX - 12.5) + 'px';
-  cursor.style.top = (event.clientY - 12.5) + 'px';
+  cursor.style.top = event.clientY - cursor.clientHeight / 2 + 'px';
+  cursor.style.left = event.clientX - cursor.clientWidth / 2 + 'px';
 };
+
+Array.from(buttons).forEach(button => {
+  button.onmouseenter = event => {
+    console.log('Entered button');
+    updateCursorSize(50);
+  };
+
+  button.onmouseleave = event => {
+    console.log('Exited button');
+    updateCursorSize(25);
+  };
+})
