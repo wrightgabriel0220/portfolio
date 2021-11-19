4c1548b2b157316c6d7ccf460787262ab0c985c7
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-11-18 21:11:54 -0800

    Refactored virtual cursor to use transforms and spin on button click

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const cursor = document.getElementById('crosshair-cursor');
 const body = document.getElementsByTagName('html')[0];
 const buttons = document.getElementsByTagName('button');
 
-const updateCursorSize = targetSize => {
-  cursor.style.height = targetSize + 'px';
-  cursor.style.width = targetSize + 'px';
-}
-
 body.onmousemove = event => {
-  cursor.style.top = event.clientY - cursor.clientHeight / 2 + 'px';
-  cursor.style.left = event.clientX - cursor.clientWidth / 2 + 'px';
+  updateCursorPosition(event.clientX, event.clientY);
 };
 
 Array.from(buttons).forEach(button => {
   button.onmouseenter = event => {
-    console.log('Entered button');
-    updateCursorSize(50);
+    cursor.style.transform = `scale(${1.5})`;
   };
 
   button.onmouseleave = event => {
-    console.log('Exited button');
-    updateCursorSize(25);
+    cursor.style.transform = `scale(${1})`;
   };
+
+  button.onmousedown = event => {
+    cursor.style.transform = `rotate(${180}deg) scale(${.5})`;
+  };
+
+  button.onmouseup = event => {
+    cursor.style.transform = `rotate(${0}deg) scale(${2})`;
+  }
 })
