9bf5a6b5dd6f314751f6adf9da77c89573941ee2
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-11-19 03:03:43 -0800

    Implemented coordinate trackers on cursor

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const updateCursorPosition = (x, y) => {
   cursor.style.left = x - cursor.clientWidth / 2 + 'px';
 };
 
+const updateTrackerPositions = (x, y) => {
+  const northSouthTracker = document.getElementById('ns-tracker');
+  const westEastTracker = document.getElementById('we-tracker');
+
+  northSouthTracker.style.left = `${x}px`;
+  westEastTracker.style.top = `${y}px`;
+
+  const southTickReporter = document.getElementById('south-tick-reporter');
+  const eastTickReporter = document.getElementById('east-tick-reporter');
+
+  southTickReporter.innerText = `x:${x}`;
+  eastTickReporter.innerText = `y:${y}`;
+}
+
 body.onmousemove = event => {
+  updateTrackerPositions(event.clientX, event.clientY);
   updateCursorPosition(event.clientX, event.clientY);
 };
 
@@ -21,10 +36,10 @@ Array.from(buttons).forEach(button => {
   };
 
   button.onmousedown = event => {
-    cursor.style.transform = `rotate(${180}deg) scale(${.5})`;
+    cursor.style.transform = `rotate(${-180}deg) scale(${.5})`;
   };
 
   button.onmouseup = event => {
-    cursor.style.transform = `rotate(${0}deg) scale(${2})`;
+    cursor.style.transform = `rotate(${0}deg) scale(${1.5})`;
   }
 })
